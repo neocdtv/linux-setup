e1c9f2e27e01cfab6f5c14d3cde641ed32ff04ac
Krzysztof Wolf <krzysztof.wolf.1980@gmail.com>
2021-12-13 12:53:18 +0100

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# next line is relvant for linux mint, where the ubuntu version is hidden in lsb-release-> $DISTRIB
 source /etc/upstream-release/lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $DISTRIB_CODENAME stable"
@@ -6,7 +7,10 @@ apt-get update
 apt-get install -y docker-ce
 echo "{\"experimental\": true}" >> /etc/docker/daemon.json
 
-curl --progress-bar -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+apt install -y jq
+DOCKER_COMPOSE_LATEST=`curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq -r .tag_name`
+curl --progress-bar -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-usermod -aG docker vagrant
+usermod -aG docker $SUDO_USER
+service docker restart
