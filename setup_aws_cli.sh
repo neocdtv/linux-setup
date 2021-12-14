204a0e120af25816f10186dcbcdcf16874e69bd2
neocdtv <krzysztof.wolf.1980@gmail.com>
2021-12-14 06:20:45 -0500

    add awscliv2

---
+++ setup_aws_cli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+AWS_CLI_NAME=awscli-exe-linux-x86_64
+AWS_CLI_NAME_EXTRACTED=aws
+AWS_CLI_HOME=$HOME/apps/awscliv2
+mkdir -p $AWS_CLI_HOME
+cd $AWS_CLI_HOME
+if [ ! -f "$AWS_CLI_NAME.zip" ]; then
+    curl --progress-bar -L -O "https://awscli.amazonaws.com/$AWS_CLI_NAME.zip"
+fi
+if [ ! -d "$AWS_CLI_NAME_EXTRACTED" ]; then
+	unzip "$AWS_CLI_NAME.zip"
+fi
+sudo ./aws/install
