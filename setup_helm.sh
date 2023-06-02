c09e2ee54202122a0a9e2baeb2f74d16fa907db2
qxz0sri <Krzysztof.Wolf@partner.bmw.de>
2023-06-02 07:04:49 -0400

    make VERSION parametrized

--- setup_helm.sh
+++ setup_helm.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+VERSION="v3.12.0"
 HELM_DIR=$HOME/apps/helm
 mkdir -p $HELM_DIR
 cd $HELM_DIR
-curl --progress-bar -O https://get.helm.sh/helm-v3.12.0-linux-amd64.tar.gz
-tar -xzvf helm-v3.12.0-linux-amd64.tar.gz
-rm helm-v3.12.0-linux-amd64.tar.gz
-mv linux-amd64 helm-v3.12.0
-chmod -R a+rwx helm-v3.12.0
+curl --progress-bar -O "https://get.helm.sh/helm-$VERSION-linux-amd64.tar.gz"
+tar -xzvf "helm-$VERSION-linux-amd64.tar.gz"
+rm "helm-$VERSION-linux-amd64.tar.gz"
+mv linux-amd64 "helm-$VERSION"
+chmod a+rwx "helm-$VERSION"
 echo "" >> $HOME/.bashrc        
-echo "export HELM_HOME=$HELM_DIR/helm-v3.12.0"  >> $HOME/.bashrc
+echo "export HELM_HOME=$HELM_DIR/helm-$VERSION"  >> $HOME/.bashrc
 echo 'export PATH="$HELM_HOME:$PATH"'  >> $HOME/.bashrc 
 
