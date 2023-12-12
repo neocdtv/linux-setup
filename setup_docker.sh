01b807c44283e33995533daf398f7af64a0e73d2
krzysztof.wolf@emeal.nttdata.com <oS6tSXEkg+n6>
2023-12-12 17:18:29 +0100

    test

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 # next line is relvant for linux mint, where the ubuntu version is hidden in lsb-release-> $DISTRIB
 source /etc/upstream-release/lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $DISTRIB_CODENAME stable"
-sudo apt-get update
-apt-get install -y docker-ce
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#apt install -y jq
+#DOCKER_COMPOSE_LATEST=`curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq -r .tag_name`
+#curl --progress-bar -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
+
 # if you run the script mulitple times, you will get multiple entries in deamon.json, which will cause docker to throw strange errors,
 # so watch out with this line, deamon.json, needs to contain well formed json, at least {}
 # TODO: maybe clear every time deamon.json? but then you will loose other settings, so again watch out
 #echo "{\"experimental\": true}" >> /etc/docker/daemon.json
 
-apt install -y jq
-DOCKER_COMPOSE_LATEST=`curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | jq -r .tag_name`
-curl --progress-bar -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-usermod -aG docker $SUDO_USER
-chmod 666 /var/run/docker.sock
-service docker restart
+sudo usermod -aG docker $SUDO_USER
+sudo chmod 666 /var/run/docker.sock
+sudo service docker restart
