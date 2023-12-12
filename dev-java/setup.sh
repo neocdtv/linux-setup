a339d04dda104d2afec5630638e404538716c8f2
Krzysztof Wolf <krzysztof.wolf.1980@gmail.com>
2023-12-12 14:19:30 +0100

    Update setup.sh

--- dev-java/setup.sh
+++ dev-java/setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# move mkdir and chmod to separate script
+mkdir -p /home/vagrant/Apps
+mkdir -p /home/vagrant/apps
+mkdir -p /home/vagrant/Projects
+chmod -R a+rwx /home/vagrant/Apps
+chmod -R a+rwx /home/vagrant/apps
+chmod -R a+rwx /home/vagrant/Projects
 ../setup_vbox_additions.sh
 ../setup_criu.sh
 ../setup_docker.sh
