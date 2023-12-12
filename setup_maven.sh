2d389c3c830219ecfaf6dcaa7f17a4d5d00e5d87
Krzysztof Wolf <krzysztof.wolf.1980@gmail.com>
2023-12-12 14:39:06 +0100

    Update setup_maven.sh

--- setup_maven.sh
+++ setup_maven.sh
@@ -2,10 +2,10 @@
 MAVEN_DIR=$HOME/Apps/maven
 mkdir -p $MAVEN_DIR
 cd $MAVEN_DIR
-curl --progress-bar -O https://downloads.apache.org/maven/maven-3/3.9.3/binaries/apache-maven-3.9.3-bin.tar.gz
-tar -xzvf apache-maven-3.9.3-bin.tar.gz
-rm apache-maven-3.9.3-bin.tar.gz
-chmod -R a+rwx apache-maven-3.9.3
+curl --progress-bar -O https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+tar -xzvf apache-maven-3.9.6-bin.tar.gz
+rm apache-maven-3.9.6-bin.tar.gz
+chmod -R a+rwx apache-maven-3.9.6
 echo "" >> $HOME/.bashrc        
-echo "export M2_HOME=$MAVEN_DIR/apache-maven-3.9.3"  >> $HOME/.bashrc
+echo "export M2_HOME=$MAVEN_DIR/apache-maven-3.9.6"  >> $HOME/.bashrc
 echo 'export PATH="$M2_HOME/bin:$PATH"'  >> $HOME/.bashrc 
