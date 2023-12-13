673e79543e3eec04acda4bc2316453ca574661c7
krzysztof.wolf@emeal.nttdata.com <oS6tSXEkg+n6>
2023-12-13 14:31:19 +0100

    update

--- dev-java/setup.sh
+++ dev-java/setup.sh
@@ -10,7 +10,7 @@ mkdir -p $PROJECTS
 
 #sudo -u $USER ../setup_vbox_additions.sh
 sudo -u $USER ../setup_docker.sh
-#sudo -u $USER ../setup_intellij.sh
+sudo -u $USER ../setup_intellij.sh
 #sudo -u $USER ../setup_keyboard_de.sh
 #sudo -u $USER ../setup_maven.sh
 #sudo -u $USER ../setup_payara_micro.sh
