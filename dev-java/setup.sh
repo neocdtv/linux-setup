95c16b9db164ca3cc155a527ed66c2d008f5b960
krzysztof.wolf@emeal.nttdata.com <oS6tSXEkg+n6>
2023-12-13 16:39:39 +0100

    update

--- dev-java/setup.sh
+++ dev-java/setup.sh
@@ -10,13 +10,12 @@ mkdir -p $PROJECTS
 sudo chown -R "$USER:$GROUP" $APPS
 sudo chown -R "$USER:$GROUP" $PROJECTS
 
-#sudo -u $USER ../setup_vbox_additions.sh
-#sudo -u $USER ../setup_docker.sh
-#sudo -u $USER ../setup_intellij.sh
+sudo ../setup_vbox_additions.sh
+sudo -u $USER ../setup_docker.sh
+sudo -u $USER ../setup_intellij.sh
 sudo -u $USER ../setup_keyboard_de.sh
 sudo -u $USER ../setup_maven.sh
 sudo -u $USER ../setup_terraform.sh
-#sudo -u $USER ../setup_payara_micro.sh
 sudo -u $USER ../setup_scite.sh
 sudo -u $USER ../setup_vscode.sh
 sudo -u $USER ../setup_nodejs.sh
