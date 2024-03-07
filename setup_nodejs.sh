d1efe891833664ab8769801226e7b258ecf18f1e
krzysztof.wolf@emeal.nttdata.com <oS6tSXEkg+n6>
2024-03-07 13:47:47 +0100

    upgrade node from 16.13.1 to 20.9.0

--- setup_nodejs.sh
+++ setup_nodejs.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-NODE_NAME=node-v16.13.1-linux-x64
+NODE_NAME=node-v20.9.0-linux-x64
 NODE_HOME=$HOME/Apps/nodejs
 mkdir -p $NODE_HOME
 chmod -R a+rw $NODE_HOME
 cd $NODE_HOME
 if [ ! -f "$NODE_NAME.tar.xz" ]; then
-    curl --progress-bar -L -O "https://nodejs.org/dist/v16.13.1/$NODE_NAME.tar.xz"
+    curl --progress-bar -L -O "https://nodejs.org/dist/v20.9.0/$NODE_NAME.tar.xz"
 fi
 if [ ! -d "$NODE_NAME" ]; then
 	tar -xvf "$NODE_NAME.tar.xz"
 fi
-grep NODE_HOME "$HOME/.bashrc"
-if [ ! $? -eq 0 ]; then
-    echo "" >> $HOME/.bashrc
-    echo "export NODE_HOME=$NODE_HOME/$NODE_NAME" >> $HOME/.bashrc
-    echo 'export PATH="$NODE_HOME/bin:$PATH"' >> $HOME/.bashrc
-fi
+echo "" >> $HOME/.bashrc
+echo "export NODE_HOME=$NODE_HOME/$NODE_NAME" >> $HOME/.bashrc
+echo 'export PATH="$NODE_HOME/bin:$PATH"' >> $HOME/.bashrc
