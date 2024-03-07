8cdceea780eb64e703dcae02325b490ab376d7ad
krzysztof.wolf@emeal.nttdata.com <oS6tSXEkg+n6>
2024-03-07 15:19:06 +0100

    update vscode setup to use latest

--- setup_vscode.sh
+++ setup_vscode.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
-VSCODE_NAME=code_1.63.0-1638855526_amd64
-VSCODE_NAME_EXTRACTED=dbeaver
-VSCODE_HOME=$HOME/Apps/vscode
+VSCODE_VERSION=latest # or e.g. 1.83.1 
+VSCODE_NAME="latest.deb"
+VSCODE_HOME=$HOME/apps/vscode
 INSTALL_HOME=/usr/share/code
 mkdir -p $VSCODE_HOME
 cd $VSCODE_HOME
-if [ ! -f "$VSCODE_NAME.deb" ]; then
-    curl --progress-bar -L -O "https://az764295.vo.msecnd.net/stable/7db1a2b88f7557e0a43fec75b6ba7e50b3e9f77e/$VSCODE_NAME.deb"
-fi
+curl --progress-bar -L "https://update.code.visualstudio.com/$VSCODE_VERSION/linux-deb-x64/stable" -o "$VSCODE_NAME"
 
-if [ ! -d "$INSTALL_HOME" ]; then
-	sudo dpkg -i "$VSCODE_NAME.deb"
-fi
+sudo rm -rf "$INSTALL_HOME"
+sudo dpkg -i "$VSCODE_NAME"
 
 LAUNCHER=$HOME/Desktop/VSCode.desktop
 
-if [ ! -f "$LAUNCHER" ]; then
-    echo '#!/usr/bin/env xdg-open' >> $LAUNCHER
-    echo '[Desktop Entry]' >> $LAUNCHER
-    echo 'Version=1.0' >> $LAUNCHER
-    echo 'Type=Application' >> $LAUNCHER
-    echo 'Terminal=false' >> $LAUNCHER
-    echo "Icon=$INSTALL_HOME/resources/app/resources/linux/code.png" >> $LAUNCHER
-    echo "Icon[en_US]=$INSTALL_HOME/resources/app/resources/linux/code.png" >> $LAUNCHER
-    echo 'Name[en_US]=VSCode' >> $LAUNCHER
-    echo "Exec=/usr/bin/code" >> $LAUNCHER
-    echo 'Name=VSCode' >> $LAUNCHER
-
-    chmod a+rwx $LAUNCHER
-fi
-
+echo '#!/usr/bin/env xdg-open' >> $LAUNCHER
+echo '[Desktop Entry]' >> $LAUNCHER
+echo 'Version=1.0' >> $LAUNCHER
+echo 'Type=Application' >> $LAUNCHER
+echo 'Terminal=false' >> $LAUNCHER
+echo "Icon=$INSTALL_HOME/resources/app/resources/linux/code.png" >> $LAUNCHER
+echo "Icon[en_US]=$INSTALL_HOME/resources/app/resources/linux/code.png" >> $LAUNCHER
+echo 'Name[en_US]=VSCode' >> $LAUNCHER
+echo "Exec=/usr/bin/code" >> $LAUNCHER
+echo 'Name=VSCode' >> $LAUNCHER
+chmod a+rwx $LAUNCHER
