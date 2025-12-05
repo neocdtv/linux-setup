194d3d6ee792a1dfa05ebdb60b8f3ec19f4081c4
Krzysztof Wolf <krzysztof.wolf.1980@gmail.com>
2025-12-05 11:17:31 +0100

    Enhance Maven setup script with version parameter
    Updated the script to allow specifying Maven version as an argument and improved download and installation process.

--- setup_maven.sh
+++ setup_maven.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-MAVEN_DIR=$HOME/Apps/maven
-mkdir -p $MAVEN_DIR
-cd $MAVEN_DIR
-curl --progress-bar -O https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
-tar -xzvf apache-maven-3.9.6-bin.tar.gz
-rm apache-maven-3.9.6-bin.tar.gz
-chmod -R a+rwx apache-maven-3.9.6
-echo "" >> $HOME/.bashrc        
-echo "export M2_HOME=$MAVEN_DIR/apache-maven-3.9.6"  >> $HOME/.bashrc
-echo 'export PATH="$M2_HOME/bin:$PATH"'  >> $HOME/.bashrc 
+
+# Default Maven version
+MAVEN_VERSION=${1:-3.9.6}
+
+MAVEN_DIR="$HOME/Apps/maven"
+MAVEN_TAR="apache-maven-$MAVEN_VERSION-bin.tar.gz"
+MAVEN_FOLDER="apache-maven-$MAVEN_VERSION"
+MAVEN_URL="https://dlcdn.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/$MAVEN_TAR"
+
+# Create directory and download
+mkdir -p "$MAVEN_DIR"
+cd "$MAVEN_DIR"
+
+echo "Downloading Maven $MAVEN_VERSION..."
+curl --progress-bar -O "$MAVEN_URL"
+
+# Extract and clean up
+tar -xzvf "$MAVEN_TAR"
+rm "$MAVEN_TAR"
+chmod -R a+rwx "$MAVEN_FOLDER"
+
+# Update .bashrc
+echo "" >> "$HOME/.bashrc"
+echo "export M2_HOME=$MAVEN_DIR/$MAVEN_FOLDER" >> "$HOME/.bashrc"
+echo 'export PATH="$M2_HOME/bin:$PATH"' >> "$HOME/.bashrc"
+
+echo "Maven $MAVEN_VERSION installation complete."
+echo "Restart your terminal or run: source ~/.bashrc"
